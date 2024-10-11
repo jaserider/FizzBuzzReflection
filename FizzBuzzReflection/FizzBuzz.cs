516fe6ceb928e9e0ac59d9bb0f3cf87ffc508287
jaserider <jaserider@hotmail.com>
2024-10-11 16:56:09 +0100

    Version 2 - All tests passed

--- FizzBuzzReflection/FizzBuzz.cs
+++ FizzBuzzReflection/FizzBuzz.cs
@@ -2,10 +2,28 @@
 {
     public class FizzBuzz
     {
+        private List<IRuleInterface> rules = new List<IRuleInterface>();
+
+        public FizzBuzz()
+        {
+            IEnumerable<Type> childRules = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).Where(t => t.GetInterfaces().Contains(typeof(IRuleInterface)));
+
+            foreach (Type childtype in childRules)
+            {
+                rules.Add((IRuleInterface)Activator.CreateInstance(childtype));
+            }
+        }
 
         public string GetStringFromNumber(int number)
         {
-            return string.Empty;
+            foreach (IRuleInterface rule in rules)
+            {
+                if (rule.MatchesRule(number))
+                {
+                    return rule.GetFizzBuzzString();
+                }
+            }
+            return number.ToString();
         }
 
     }
---
+++ FizzBuzzReflection/Rules.cs
@@ -0,0 +1,48 @@
+﻿using FizzBuzzReflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzReflection
+{
+    public class FizzBuzzRule : IRuleInterface
+    {
+        public bool MatchesRule(int number)
+        {
+            return number % 3 == 0 && number % 5 == 0;
+        }
+
+        public string GetFizzBuzzString()
+        {
+            return "Fizzbuzz";
+        }
+    }
+
+    public class FizzRule : IRuleInterface
+    {
+        public bool MatchesRule(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        public string GetFizzBuzzString()
+        {
+            return "Fizz";
+        }
+    }
+
+    public class BuzzRule : IRuleInterface
+    {
+        public bool MatchesRule(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        public string GetFizzBuzzString()
+        {
+            return "Buzz";
+        }
+    }
+}
